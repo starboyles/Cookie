16209702570f39c22901e665a2da28001320fa19
Santan21 <lesliegyamfi02@gmail.com>
2024-03-08 17:04:23 +0000

    chore: connected server to MongoDB

--- index.js
+++ index.js
@@ -1,5 +1,24 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const mongoose = require('mongoose');
-const dotenv = require('dotenv');
-const 
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const helmet = require("helmet");
+const morgan = require("morgan");
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((error) => {
+    console.error("Error connecting to MongoDB", error);
+  });
+
+app.listen(8000, () => {
+  console.log("Backend running on port 8000");
+});
